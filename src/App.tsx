394525f8c98d5501473bc4361c9435810351f9b5
Dmitry Sentyurev <dmitry.sent@gmail.com>
2023-05-28 19:45:11 +0300

    add React-router-dom, Code splitting, lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from 'react-router-dom';
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 export const App = () => {
     return (
-        <div>
-            <p className='app'>Test 123465</p>
-            <Counter />
+        <div className='app'>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
