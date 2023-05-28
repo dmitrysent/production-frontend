d32732bf748fd344f6d5c0c2c7e940f831adceab
Dmitry Sentyurev <dmitry.sent@gmail.com>
2023-05-28 10:23:56 +0300

    add css-modules

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Counter} from "./components/Counter";
+import './index.scss'
+
+export const App = () => {
+    return (
+        <div>
+            <p className='app'>Test 123465</p>
+            <Counter />
+        </div>
+    );
+};
